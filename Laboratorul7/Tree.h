c023b99788d30e4015431a0bea8793ec83c6a1a2
andrei-mrd <andreidanielmardare@gmail.com>
2024-04-23 19:41:34 +0300

    Lab7

---
+++ Laboratorul7/Tree.h
@@ -0,0 +1,144 @@
+#pragma once
+
+template <class T>
+struct nod {
+public:
+    int cnt = 0; // nr de copii
+    T info;
+    nod** children; // pointer la pointer
+};
+
+template <class T>
+class Tree {
+    nod<T>* root;
+public:
+    Tree() {
+        this->root = nullptr;
+    }
+
+    void add_node(nod<T>*& parent, const T& value) {
+        if (parent == nullptr) {
+            parent = new nod<T>;
+            parent->info = value;
+            parent->children = nullptr;
+            this->root = parent;
+        }
+        else {
+            nod<T>** new_children = new nod<T>*[parent->cnt + 1];
+
+            for (int i = 0; i < parent->cnt; i++) {
+                new_children[i] = parent->children[i];
+            }
+
+            new_children[parent->cnt] = new nod<T>;
+            new_children[parent->cnt]->info = value;
+            new_children[parent->cnt]->children = nullptr;
+            new_children[parent->cnt]->cnt = 0;
+
+            delete[] parent->children;
+            parent->children = new_children;
+            parent->cnt++;
+        }
+    }
+
+    void get_node(nod<T>* parent, const T& value) {
+        if (parent == nullptr) {
+            return this->root;
+        }
+        else {
+            for (int i = 0; i < parent->cnt; i++) {
+                if (parent->children[i]->info == value) {
+                    return parent->children[i]->info;
+                }
+            }
+            return nullptr;
+        }
+    }
+
+    void print(nod<T>* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        std::cout << node->info << " ";
+
+        for (int i = 0; i < node->cnt; i++) {
+            print(node->children[i]);
+        }
+    }
+
+    nod<T>* find(nod<T>* node, const T& value, int (*callback)(T, T)) {
+        if (node == nullptr)
+            return nullptr;
+        if (callback(node->info, value)) {
+            return node;
+        }
+
+        for (int i = 0; i < node->cnt; i++) {
+            nod<T>* p = find(node->children[i], value, callback);
+            if (p)
+                return p;
+        }
+
+        return nullptr;
+    }
+
+    void insert(nod<T>*& parent, int index, const T& value) {
+        nod<T>** p = new nod<T>*[parent->cnt + 1];
+        p[parent->cnt] = new nod<T>;
+        for (int i = 0; i < index; i++) {
+            p[i] = parent->children[i];
+        }
+        for (int i = index + 1; i < parent->cnt + 1; i++) {
+            p[i] = parent->children[i - 1];
+        }
+        delete[] parent->children;
+        nod<T>* k = new nod<T>;
+        k->info = value;
+        k->children = nullptr;
+        p[index] = k;
+        parent->children = p;
+        parent->cnt++;
+
+    }
+
+    void sort(nod<T>*& parent, int (*callback)(T, T)) {
+        for (int i = 0; i < parent->cnt - 1; i++) {
+            for (int j = i + 1; j < parent->cnt; j++) {
+                if (callback(parent->children[i]->info, parent->children[j]->info) == -1) {
+                    std::swap(parent->children[i], parent->children[j]);
+                }
+                else if (callback == nullptr) {
+                    if (parent->children[i]->info > parent->children[j]->info) {
+                        std::swap(parent->children[i], parent->children[j]);
+                    }
+                }
+            }
+        }
+    }
+
+    /*not implemented*/
+    int count(nod<T>* node) {
+        int i = 0;
+        while (node->children[i] != nullptr) {
+            //std::cout << node->children[i]->info << " ";
+            i++;
+        }
+        return i;
+    }
+
+    ~Tree() {
+        delete_node(this->root);
+    }
+
+private:
+    void delete_node(nod<T>* node) {
+        if (node == nullptr)
+            return;
+        for (int i = 0; i < node->cnt; i++) {
+            delete_node(node->children[i]);
+        }
+        delete[] node->children;
+        delete node;
+    }
+};
--- Laboratorul7/main2.cpp
+++ Laboratorul7/main2.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
-#include "Vector.h"
+#include "Tree.h"
+using namespace std;
 
-int compare_ints(int x, int y) {
-	if (x < y)
-		return -1;
-	if (x > y)
-		return 1;
-	return 0;
+float operator"" _Kelvin(unsigned long long int a) {
+    return (float)a - 273.15;
 }
 
-int main() {
-    Vector<int> v;
+float operator"" _Fahrenheit(unsigned long long int a) {
+    return (float)a * 9.0 / 5.0 + 32.0;
+}
 
-    // index, element
-    v.insert(0, 10);
-    v.insert(1, 5);
-    v.insert(2, 20);
+int compare(int a, int b) {
+    if (a == b)
+        return 1;
+    else if (a > b)
+        return -1;
+    return 0;
+}
 
-    Vector<int> w = v;
+int main()
+{
+    float a = 320_Kelvin;
+    float b = 120_Fahrenheit;
+    //cout << a << " " << b;
 
-    // index
-    v.remove(0);
-    v.sort(compare_ints);
-    printf("%d\n", w[0]);
-
-    v.print();
-	return 0;
+    Tree<int> c;
+    nod<int>* root = nullptr;
+    c.add_node(root, 5);
+    c.add_node(root, 10);
+    c.add_node(root, 12);
+    c.add_node(root, 5);
+    c.add_node(root, 18);
+    c.add_node(root, 8);
+    //c.print(root);
+    //std::cout << "\n";
+    c.insert(root, 2, 7);
+    //std::cout << "\n";
+     c.print(root);
+    //std::cout << "\n";
+    //c.sort(root, compare);
+    //c.print(root);
+    std::cout << "\n";
+    //std::cout << c.count(root);
+    // std::cout<<c.find(root, 10, compare)->info;
 }
